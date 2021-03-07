2292301e7c966b10fa580b837c2b8f455406a5f4
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-03-07 15:20:29 -0800

    Implement {extended test cases upto 64}

--- p1_main.py
+++ p1_main.py
@@ -3,10 +3,10 @@ import p1_fizzbuzz
 
 class FizzBuzzTest(unittest.TestCase):
   def test_fizzBuzz1(self):
-    self.assertEqual(self.test_case(), p1_fizzbuzz.output(1, 100))
+    self.assertEqual(self.test_case(), p1_fizzbuzz.output(1, 67))
 
   def test_case(self):
-    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n'
+    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n'
 
 if __name__ == '__main__':
     unittest.main()
