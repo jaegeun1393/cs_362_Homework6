701a115cd7b0e0deb9e31ca1267158423b83181c
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-03-07 15:16:17 -0800

    Edited the testcase{add \n at the end of the test}

--- p1_main.py
+++ p1_main.py
@@ -6,7 +6,7 @@ class FizzBuzzTest(unittest.TestCase):
     self.assertEqual(self.test_case(), p1_fizzbuzz.output(1, 35))
 
   def test_case(self):
-    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34'
+    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\n'
 
 if __name__ == '__main__':
     unittest.main()
