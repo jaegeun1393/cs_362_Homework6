1a562eb09f9d2a9d3588c98c74926d3f9eceac72
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-03-07 15:55:58 -0800

    Implement{add true return when if cases all meets}

--- p2_leap.py
+++ p2_leap.py
@@ -1,5 +1,11 @@
 def output(year):
   if (year % 4) == 0:
-    return True
+    if (year % 100) == 0:
+      if (year % 400) == 0:
+        return True
+      else:
+        return False
+    else:
+       return True
   else:
     return False
