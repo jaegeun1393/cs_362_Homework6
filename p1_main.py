e7592071c91d137838b2b8f4891dd2dca4618d4a
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-03-07 15:12:23 -0800

    Extended test cases

--- p1_main.py
+++ p1_main.py
@@ -3,10 +3,10 @@ import p1_fizzbuzz
 
 class FizzBuzzTest(unittest.TestCase):
   def test_fizzBuzz1(self):
-    self.assertEqual(self.test_case(), p1_fizzbuzz.output(1, 12))
+    self.assertEqual(self.test_case(), p1_fizzbuzz.output(1, 35))
 
   def test_case(self):
-    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\n'
+    return '1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34'
 
 if __name__ == '__main__':
     unittest.main()
