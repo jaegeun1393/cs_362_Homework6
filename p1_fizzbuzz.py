b835c356566847b404426286013f021ba1250339
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-03-07 15:15:41 -0800

    Implemented FizzBuzz method {when i = 3 and 5}

--- p1_fizzbuzz.py
+++ p1_fizzbuzz.py
@@ -1,7 +1,9 @@
 def output(a, b):
   x = ''
   for i in range(a, b):
-    if i%3==0:
+    if i%3==0 and i%5==0:
+      x = x + 'FizzBuzz'
+    elif i%3==0:
       x = x + 'Fizz'
     elif i%5==0:
       x = x + 'Buzz'
